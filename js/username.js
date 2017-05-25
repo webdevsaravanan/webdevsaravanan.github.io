59d614ce861cd8c73e4f6b3aa07c1888a13ae9bc
webdevsaravanan <webdevsaravanan>
2017-05-25 22:42:35 +0530

    name updated

--- js/username.js
+++ js/username.js
@@ -1,14 +1,20 @@
-$(document).ready(function(){
-		$('#username').fadeOut(4000);
-		setTimeout("$('#username').text('webdevsaravanan')",5000);
-		$('#fe').fadeOut('fast');
+     /*   $('#fe').fadeOut('fast');
 		$('#js').fadeOut('fast');
 		$('#php').fadeOut('fast');
 		$('#cs').fadeOut('fast');
-
-		setTimeout("$('#username').fadeIn(4000)",6000);
+$(document).ready(function(){
+		$('#username').fadeOut(4000);
+		setTimeout(function(){
+			//$('#username').text('webdevsaravanan');
+			//$('#username').hide();
+			fade();
+		},5000);
+		function fade(){
+			setTimeout("$('#username').fadeIn(6000)",4000);
+		}
 		$('#fe').fadeIn(3000);
 		$('#js').fadeIn(5000);
 		$('#php').fadeIn(7000);
 		$('#cs').fadeIn(9000);
 });
+*/
